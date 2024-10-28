6942ab99d8508432b8e74fb582a987c8b70a6c89
Zikdev <thaodubai122004@gmail.com>
2024-10-28 22:56:03 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+function randomText(){
+    var text = ("!@#$%^*()")
+    letters =text[Math.floor(Math.random() * text.length)];
+    return letters;
+}
+
+function rain() {
+    let cloud = document.querySelector('.cloud');
+    let e = document.createElement('div');
+    e.classList.add('drop');
+    cloud.appendChild(e);
+
+    let left = Math.floor(Math.random() * 300)
+    let size = Math.random() * 1.5;
+    let duration = Math.random() *1;
+
+    e.innerText = randomText();
+    e.style.left = left + 'px';
+    e.style.fontSize = 0.5+size +'em';
+    e.style.animationDuration = 1+duration+'s';
+
+      setTimeout(function(){
+      cloud.removeChild(e)
+      },2000)
+}
+
+setInterval(function(){
+    rain()
+},20);
+
+// The cloud effect completes here.
